f228bb5aba3ea6cc964acb8d24af89dc1186db70
Steve Ryan <stryan@terpmail.umd.edu>
2017-01-17 17:33:23 -0500

    added targeted tweeting

--- autoresponder.py
+++ autoresponder.py
@@ -31,12 +31,19 @@ class AutoResponder():
                 sentence = self.model.make_short_sentence(140 - limit)
                 full_sentence = "@" + uname + " " + sentence
                 print "Responding with: " + full_sentence
-                self.responses.append(tweet.id)  
                 if (not self.dummy):
-                   
+                   self.responses.append(tweet.id)  
                    self.api.update_status(full_sentence,tweet.id) 
         return
-    
+    def target_tweet(self):
+        print "Target who? "
+        target = raw_input()
+        sentence = self.model.make_short_sentence(140 - (len(target) + 2))
+        full_sentence = "@"+target+" " + sentence
+        print "Tweeting: " + full_sentence
+        if (not self.dummy):
+            self.api.update_status(full_sentence)
+
     def save_responses(self):
         with open("responded.txt",'wb') as fp:
             pickle.dump(self.responses,fp)
@@ -52,6 +59,8 @@ class AutoResponder():
             if cmd == "current_rate":
                 json_rate = self.api.rate_limit_status()
                 print json.dumps(json_rate, indent=2)
+            if cmd == "target":
+                self.target_tweet()
             if cmd == "toggle_dummy":
                 if self.dummy == True:
                     self.dummy = False
